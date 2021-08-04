c17c8bfc9302dd7caeb00a67167dfeaa0309beab
jhs512 <jangka512@gmail.com>
2021-08-04 11:42:34 +0900

    타임리프 layout 적용

--- src/main/kotlin/com/jhs/wiken/WikenApplication.kt
+++ src/main/kotlin/com/jhs/wiken/WikenApplication.kt
@@ -1,10 +1,18 @@
 package com.jhs.wiken
 
+import nz.net.ultraq.thymeleaf.LayoutDialect
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+
 
 @SpringBootApplication
 class WikenApplication
+
+@Bean
+fun layoutDialect(): LayoutDialect? {
+	return LayoutDialect()
+}
 
 fun main(args: Array<String>) {
 	runApplication<WikenApplication>(*args)
