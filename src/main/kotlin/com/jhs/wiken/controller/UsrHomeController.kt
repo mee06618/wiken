c2ce21d246360794479a30313af7e959caa9371e
jhs512 <jangka512@gmail.com>
2021-08-09 16:39:00 +0900

    로그인 화면 꾸미기

--- src/main/kotlin/com/jhs/wiken/controller/UsrHomeController.kt
+++ src/main/kotlin/com/jhs/wiken/controller/UsrHomeController.kt
@@ -1,12 +1,23 @@
 package com.jhs.wiken.controller
 
 import com.jhs.wiken.vo.Rq
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class UsrHomeController {
+    @Autowired
+    private lateinit var rq: Rq;
+
+    @RequestMapping("/")
+    @ResponseBody
+    fun showRoot(): String {
+        return rq.replaceJs("", "/ken")
+    }
+
     @RequestMapping("/home/main")
     fun showMain(model: Model): String {
         model.addAttribute("rq", Rq())
